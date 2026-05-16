e77a34b6b83ac090f4e68d70f3b64e638d6742b8
Vitor Silva <vitor.pro@outlook.com>
2026-05-16 19:41:11 +0100

    chore: allow local frontend origin for ticket endpoints

--- src/main/java/com/vitorsilva/helpdesk_api/controller/TicketController.java
+++ src/main/java/com/vitorsilva/helpdesk_api/controller/TicketController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tickets")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TicketController {
     private final TicketService ticketService;
 
