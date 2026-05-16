c571932b1ef61e7f9b54236a1db0af75a994e8a8
Vitor Silva <vitor.pro@outlook.com>
2026-05-16 14:24:08 +0100

    feat: add ticket entity

---
+++ src/main/java/entity/Ticket.java
@@ -0,0 +1,40 @@
+package entity;
+
+import enums.TicketPriority;
+import enums.TicketStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    String description;
+    @Enumerated(EnumType.STRING)
+    TicketStatus status;
+    @Enumerated(EnumType.STRING)
+    TicketPriority priority;
+    String requesterName;
+    String requesterEmail;
+    String assignedTo;
+    LocalDateTime createdAt;
+    LocalDateTime updateAt;
+
+}
