0426d61af431a53f5504e1f598385f8c5a93d9fb
Vitor Silva <vitor.pro@outlook.com>
2026-05-16 14:14:06 +0100

    feat: add ticket status and priority enums

---
+++ src/main/java/enums/TicketPriority.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum TicketPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/enums/TicketStatus.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum TicketStatus {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED
+}
